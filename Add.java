ef9683e848cf40310f9bdf0fadb05b479542af69
Mayanks1203 <168633100+Mayanks1203@users.noreply.github.com>
2025-04-03 15:26:37 +0530

    Update Add.java

--- Add.java
+++ Add.java
@@ -5,7 +5,7 @@ public static void main(String[]args){
 Scanner sc=new Scanner(System.in);
 Integer i=sc.nextInt();
 Integer j=sc.nextInt();
-
+// addd for check11
 Integer k=i+j;
 System.out.println("Add:-"+k);
 
