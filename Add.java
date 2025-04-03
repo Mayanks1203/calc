92868b5dca9069fa6857cb0f9933d7a5235ab721
Mayank Sahu <sahumayank689@gmail.com>
2025-04-03 15:22:13 +0530

    Addition sucess

---
+++ Add.java
@@ -0,0 +1,15 @@
+import java.util.*
+
+Class Add{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+Integer i=sc.nextInt();
+Integer j=sc.nextInt();
+
+Integer k=i+j;
+System.out.println("Add:-"+k);
+
+
+}
+}
+
